eb6084ccbc0fb30833bd5fd59cea7d1b4a17f114
Zohaib <zohaibk1111@gmail.com>
2020-07-17 11:05:43 -0400

    services folder set up. apiConfig file create

---
+++ client/src/services/profile.js
@@ -0,0 +1,37 @@
+import api from "./apiConfig";
+
+export const getProfile = async (id) => {
+  try {
+    const response = await api.get(`/profile/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createProfile = async (profile) => {
+  try {
+    const response = await api.post("/profile", profile);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProfile = async (id, profile) => {
+  try {
+    const response = api.put(`/profile/${id}`, profile);
+    response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProfile = async (id) => {
+  try {
+    const response = await api.delete(`/profile/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
