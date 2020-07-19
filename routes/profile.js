45b6b7dddf2f2c23192013fdd896be777e9f019f
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-07-19 14:45:46 -0400

    debug

--- routes/profile.js
+++ routes/profile.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get("/profiles", controllers.getProfiles);
 router.get("/profile/:id", controllers.getProfile);
 router.post("/profile/create", controllers.createProfile);
-router.put("/profile/:id", controllers.updateProfile);
+router.put("/profile/update/:id", controllers.updateProfile);
 router.delete("/profile/:id", controllers.deleteProfile);
 
 module.exports = router;
