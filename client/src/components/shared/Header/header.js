94a7e9607a0b8a41352a4ac0f863415f166b31c5
Brian Farr <64746326+BriansFarr@users.noreply.github.com>
2020-07-21 18:09:48 -0400

    nav bar fixes

--- client/src/components/shared/Header/header.js
+++ client/src/components/shared/Header/header.js
@@ -5,12 +5,12 @@ import './header.css'
 function Header(props) {
   return (
     <header>
-       <a href="/home"><p  className='logo'>Hitch</p></a>
+       <a href="/"><p  className='logo'>Hitch</p></a>
       <ul>
         <li className='one'>Find My Ride</li>
         <li className='two'>About Us</li>
         <li className='three'>Contact Us</li>
-        <a className="link" a href="/profile/id"><li className='four'>My Account</li></a>
+        <li><a className="four" a href="/profile/id">My Account</a></li>
         <li className='five'>Sign in</li>
       </ul>
     </header>
