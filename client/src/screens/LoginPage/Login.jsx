69b7658ee4867cddaa1dcb24c2613032853025c8
Zohaib <zohaibk1111@gmail.com>
2020-07-23 09:13:50 -0400

    Working on responsiveness of app

--- client/src/screens/LoginPage/Login.jsx
+++ client/src/screens/LoginPage/Login.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import "../LoginPage/Login.css"
 import {getProfile} from "../../services/profile"
 import Layout from "../../components/shared/Layout/Layout"
+import {Link} from "react-router-dom"
 
 class Login extends Component {
     constructor() {
@@ -11,22 +12,44 @@ class Login extends Component {
             profile: {
                 emailAddress:'',
                 password: ''
-            }
+            },
+            signIn: false,
         }
 
     }
 
 
 
+
     render(){
         return(
-            <div className = "main-login-container">
+            
                 <Layout>
-                    <h1>Login Pag</h1>
+                <div className = "main-login-container">
+                    <div className="sign-in-option-container">
+                        <h1 className="signin-header">Sign In</h1>
+                        <form>
+                         <label>School Email:</label>
+                         <input type="text"/>
+                         <label>Password:</label>
+                         <input type="text" />
+                         <label>Forgot Password?</label>
+
+                        <button>Sign In</button>
+                        </form>
+                    </div>
+                    <div className="create-account">
+                        <p className="create-account-header combination">Do you have an Account?</p>
+                        <Link  to="/create/profile"><p className ="create-account-link combination">Create an Account</p></Link>
+
+
+                    </div>
 
                     
+
+                </div>
                 </Layout>
-            </div>
+            
         )
     }
 }
